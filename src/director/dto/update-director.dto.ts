4241987f033e6614ba1d47c858776e3247b7c763
seocho507 <gram753@naver.com>
2024-10-01 19:50:15 +0900

    feat : Mapped Types를 적용하여 dto 정리

--- src/director/dto/update-director.dto.ts
+++ src/director/dto/update-director.dto.ts
@@ -1,20 +1,5 @@
 import {PartialType} from "@nestjs/mapped-types";
 import {CreateDirectorDto} from "./create-director.dto";
-import {IsDateString, IsNotEmpty, IsOptional, IsString} from "class-validator";
 
 export class UpdateDirectorDto extends PartialType(CreateDirectorDto) {
-    @IsNotEmpty()
-    @IsOptional()
-    @IsString()
-    name?: string;
-
-    @IsNotEmpty()
-    @IsOptional()
-    @IsDateString()
-    dateOfBirth?: Date;
-
-    @IsNotEmpty()
-    @IsOptional()
-    @IsString()
-    nationality?: string;
 }
